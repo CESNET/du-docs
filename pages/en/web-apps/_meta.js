33d2e76883f1d4890664ca9350d8c45b8f3c235a
viktoriaas <viktoria.spisakovaa@gmail.com>
2024-09-23 17:57:26 +0200

    nbgrader: remove old student page

--- pages/en/web-apps/_meta.js
+++ pages/en/web-apps/_meta.js
@@ -2,6 +2,5 @@ export default {
         "alphafold": "Alphafold",
         "binderhub": "BinderHub",
         "jupyterhub": "JupyterHub",
-        "nbgrader-new-interaction": "JupyterHub - NBGrader",
-        "nbgrader-auth-student": "JupyterHub - NBGrader - Auth"
+        "nbgrader-new-interaction": "JupyterHub - NBGrader"
 }
